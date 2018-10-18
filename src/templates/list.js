d2637e9abcc93f77f356bd7a8509955072d570ee
Pallavi <pshukla6@sapient.com>
2018-10-18 17:12:34 +0530

    XT exercise - HBS

---
+++ src/templates/list.js
@@ -0,0 +1,13 @@
+this["MyApp"] = this["MyApp"] || {};
+this["MyApp"]["templates"] = this["MyApp"]["templates"] || {};
+this["MyApp"]["templates"]["list"] = Handlebars.template({"1":function(container,depth0,helpers,partials,data) {
+    return "  <li>"
+    + container.escapeExpression(container.lambda(depth0, depth0))
+    + "</li>\n";
+},"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
+    var stack1;
+
+  return "<ul>\n"
+    + ((stack1 = helpers.each.call(depth0 != null ? depth0 : (container.nullContext || {}),(depth0 != null ? depth0.employees : depth0),{"name":"each","hash":{},"fn":container.program(1, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "")
+    + "</ul>";
+},"useData":true});
