3047ac874e6fd2d2d9ad78c26c66e03297e00f22
Pallavi <pshukla6@sapient.com>
2018-10-30 16:09:10 +0530

    untangle-xtproject-part2

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'testing' });
+  res.render('index', { title: 'testing22' });
 });
- 
+  
 module.exports = router;
